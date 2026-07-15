f1ff0009ea540902d5a29c3d5f9bee17e50a27f6
ic <a@b.c>
2026-07-15 09:09:58 +0800

    add .gitignore, drop pycache

--- Formula/interrupt-capture.rb
+++ Formula/interrupt-capture.rb
@@ -3,9 +3,9 @@ class InterruptCapture < Formula
 
   desc "Local plain-text CLI to capture & resume what you were doing when interrupted"
   homepage "https://github.com/muyunye/interrupt-capture"
-  url "file:///tmp/interrupt-capture-0.1.0.tar.gz"
-  sha256 "636830f9d9f681dbe5957dafa1d13a01a211f50815e537e3947da036c652a127"
-  version "0.1.0"
+  url "file:///tmp/interrupt-capture-0.2.0.tar.gz"
+  sha256 "67dc18ba265eafa061c99e1a610c3c5018a6959013c668cbbab82a300b29fd51"
+  version "0.2.0"
   license "MIT"
 
   depends_on "python@3.12"
